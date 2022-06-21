e36cccdd3c4fcc87cefd704c15136e90a891dcbd
Abi02GB <abigailgalzin@gmail.com>
2022-06-21 01:45:24 -0400

    Implementacion funcionalidad en el boton de preinscripcion de home

--- Interfaz/src/app/modules/home/body-home/body-home.component.ts
+++ Interfaz/src/app/modules/home/body-home/body-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-body-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  clickRPHome () {
+    this.router.navigate(['/registro-delegado']);
+  };
+  
 }
