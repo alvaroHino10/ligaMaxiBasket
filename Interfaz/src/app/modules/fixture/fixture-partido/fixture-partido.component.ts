e2a267f1d085298baab441e9093bada8698190ce
Abi02GB <abigailgalzin@gmail.com>
2022-06-24 23:44:30 -0400

    Implementacion de mensajes personalizados, validacion diferentes equipos y jueces

--- Interfaz/src/app/modules/fixture/fixture-partido/fixture-partido.component.ts
+++ Interfaz/src/app/modules/fixture/fixture-partido/fixture-partido.component.ts
@@ -16,6 +16,29 @@ export class FixturePartidoComponent implements OnInit {
   listaEquipos: any = [];
   listaArbitros: any = [];
   listaApuntadorMesa: any = [];
+
+  listaEquiposfake: any = [this.setRegistro("Cocos", "", "", ""),
+  this.setRegistro("Locos", "", "", ""),
+  this.setRegistro("Pedros", "", "", ""),
+  this.setRegistro("Paules", "", "", ""),
+  this.setRegistro("Los pumas azules", "", "", ""),
+  this.setRegistro("Gatos verdes", "", "", ""),
+  this.setRegistro("Pueblo azul", "", "", "")
+  ];
+  listaArbitrosfake: any = [this.setRegistro("", "martinez2", "flores", "arbitro"),
+  this.setRegistro("", "martinez3", "flores", "arbitro"),
+  this.setRegistro("", "martinez4", "flores", "arbitro"),
+  this.setRegistro("", "martinez5", "flores", "arbitro"),
+  this.setRegistro("", "martinez6", "flores", "arbitro"),
+  ];
+  listaAMfake: any = [this.setRegistro("", "martinez", "flores0", "mesa"),
+  this.setRegistro("", "martinez", "flores1", "mesa"),
+  this.setRegistro("", "martinez", "flores2", "mesa"),
+  this.setRegistro("", "martinez", "flores3", "mesa"),
+  this.setRegistro("", "martinez", "flores4", "mesa"),
+  this.setRegistro("", "martinez", "flores5", "mesa"),
+  ];
+
   time: NgbTimepicker;
 
   constructor(public activeModal: NgbActiveModal,
@@ -25,20 +48,20 @@ export class FixturePartidoComponent implements OnInit {
       equipo1: new FormControl('',
         Validators.required),
       equipo2: new FormControl('',
-        Validators.required), 
+        Validators.required),
       fechaPartido: new FormControl('',
         Validators.required),
       horaPartido: new FormControl('',
         Validators.required),
-      lugarPartido: new FormControl('',[
+      lugarPartido: new FormControl('', [
         Validators.required,
         Validators.minLength(3)]),
       primerJuez: new FormControl('',
-      Validators.required),
-      segundoJuez: new FormControl ('',
-      Validators.required),
+        Validators.required),
+      segundoJuez: new FormControl('',
+        Validators.required),
       apuntadorMesa: new FormControl('',
-      Validators.required)
+        Validators.required)
     });
   }
 
@@ -54,9 +77,35 @@ export class FixturePartidoComponent implements OnInit {
     });
   }
 
-  crearPartido(){
+  crearPartido() {
     this.submitted = true;
   }
 
+  get equiposIguales(){
+    if(this.formPartido.value.equipo1 == "" || this.formPartido.value.equipo2 == ""){
+      return false;
+    }
+    return (this.formPartido.value.equipo1 == this.formPartido.value.equipo2);
+  }
+
   get controls() { return this.formPartido.controls; }
+
+  get arbitrosIguales(){
+    if(this.formPartido.value.primerJuez == "" || this.formPartido.value.segundoJuez == ""){
+      return false;
+    }
+    return (this.formPartido.value.primerJuez== this.formPartido.value.segundoJuez);
+  }
+
+  //datos fake
+  setRegistro(nombreEqui: any, nombre: any, p_Ap: any, rol: any) {
+    const registroJugador = {
+      nombre_equi: nombreEqui,
+      nombre_cp: nombre,
+      prim_ap_cp: p_Ap,
+      rol_cp: rol,
+    };
+    return registroJugador;
+  }
+
 }
