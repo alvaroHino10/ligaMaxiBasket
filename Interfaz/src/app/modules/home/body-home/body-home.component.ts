8c899dc8236efb4f3b5ea9710a4cefb616499e78
Paulm009 <poolpos8@gmail.com>
2022-06-26 21:05:58 -0400

    integracion de las estadisticas de los jugadores en las tablas de equipos

--- Interfaz/src/app/modules/home/body-home/body-home.component.ts
+++ Interfaz/src/app/modules/home/body-home/body-home.component.ts
@@ -11,10 +11,19 @@ export class BodyHomeComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    console.log(this.fecha),
+    console.log(this.añoActual),
+    console.log(this.hoy),
+    console.log(this.mesActual)
   }
 
   clickRegistroHome () {
     this.router.navigate(['/registro-delegado']);
   };
   
+  fecha = new Date();
+  añoActual = this.fecha.getFullYear();
+   hoy = this.fecha.getDate();
+  mesActual = this.fecha.getMonth() + 1; 
+
 }
