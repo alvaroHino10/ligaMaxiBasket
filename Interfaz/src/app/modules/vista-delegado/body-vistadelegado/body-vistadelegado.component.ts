7079a9c7633ef5b8bcc0afe15aba0c27aa967d1a
Abi02GB <abigailgalzin@gmail.com>
2022-06-28 01:35:11 -0400

    Mejora en vista, falta ruteo de botones

--- Interfaz/src/app/modules/vista-delegado/body-vistadelegado/body-vistadelegado.component.ts
+++ Interfaz/src/app/modules/vista-delegado/body-vistadelegado/body-vistadelegado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-body-vistadelegado',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyVistadelegadoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  registroPreinscripcion(){
+    this.router.navigate(['registro-preinscripcion'])
+  }
+
 
 }
